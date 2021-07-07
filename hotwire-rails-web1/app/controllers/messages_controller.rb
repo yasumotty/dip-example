c71847904821f274fd1665bbc306d8749ed04e9c
Takahiro Yasumoto <takahiro.yasumoto@readyfor.jp>
2021-07-07 09:19:45 +0900

    initial commit

---
+++ hotwire-rails-web1/app/controllers/messages_controller.rb
@@ -0,0 +1,30 @@
+class MessagesController < ApplicationController
+  before_action :set_room, only: [:new, :create]
+
+  # GET /rooms/new
+  def new
+    @message = @room.messages.new
+  end
+
+  # POST /rooms
+  # POST /rooms.json
+  def create
+    @message = @room.messages.create!(message_params)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to @room }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_room
+      @room = Room.find(params[:room_id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def message_params
+      params.require(:message).permit(:content)
+    end
+end
